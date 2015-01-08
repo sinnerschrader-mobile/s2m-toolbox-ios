b17865ed2711eb9d13cd12d2582d8d656f9eba34
François Benaiteau <francois.benaiteau@sinnerschrader-mobile.com>
2015-01-08 11:02:04 +0100

    Initial Commit

---
+++ Example/testcocoapods/testcocoapods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testcocoapods
+//
+//  Created by François Benaiteau on 08/01/15.
+//  Copyright (c) 2015 Sinnerschrader Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
