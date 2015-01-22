970dda59e4958a86bca6267979ab026e3582b33d
Park Sanggeon <gunnih@gmail.com>
2015-01-22 17:06:38 +0100

    add comment for public API

--- LocalNotificationHelper/UILocalNotification+S2MNotificationHelper.h
+++ LocalNotificationHelper/UILocalNotification+S2MNotificationHelper.h
@@ -9,6 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface UILocalNotification (S2MNotificationHelper)
+
+// set key for internal usage of S2MNotificationHelper
+// to use S2MNotificationHelper, it has to be used.
 - (void)setS2mKey:(NSString *)key;
+
+// get key for internal usage of S2MNotificationHelper
 - (NSString *)s2mKey;
 @end
