54f13c35ee0c289d3eaac8122b9669dfe51cd6e6
François Benaiteau <francois.benaiteau@sinnerschrader-mobile.com>
2015-01-26 17:46:07 +0100

    Document header

--- Webview/S2MWebViewController.h
+++ Webview/S2MWebViewController.h
@@ -9,11 +9,25 @@
 #import <UIKit/UIKit.h>
 
 @interface S2MWebViewController : UIViewController
-@property (nonatomic, strong, readonly) UIWebView *webView;
-@property (nonatomic, assign) BOOL shouldOpenLinks;
+@property (nonatomic, strong, readonly) UIWebView *webView;/// webview used to load given URL.
+@property (nonatomic, assign) BOOL shouldOpenLinks;/// will let application open links when a local URL is loaded
 
 -(id)init __attribute__((unavailable("This method is not available, use initWithURL: instead")));
+/**
+ *  Loads and display given URL
+ *
+ *  @param url remote URL of the resource to load
+ *
+ *  @return instance of S2MWebViewController
+ */
 -(instancetype)initWithURL:(NSURL*)url NS_DESIGNATED_INITIALIZER;
+/**
+ *  Loads and display given local URL
+ *
+ *  @param file filename of resource contained in bundle of application
+ *
+ *  @return instance of S2MWebViewController
+ */
 -(instancetype)initWithBundleFile:(NSString*)file;
 
 @end
